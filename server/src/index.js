0ec4cdda220dfeacef4af1539137d369ef6ee519
Hugo <hugo.berthome@epitech.eu>
2021-06-01 11:08:42 +0900

    A client can move a robot if he is master

--- server/src/index.js
+++ server/src/index.js
@@ -91,7 +91,6 @@ io.on('connection', (socket) => {
         }
     });
 
-
     /* client connect to robot for control */
     socket.on("connect to robot", (id) => {
         if (socket.type === "client") {
@@ -105,6 +104,24 @@ io.on('connection', (socket) => {
             disconnectFromRobot(io, socket);
         }
     });
+
+    // move a robot from master socket
+    socket.on("move", (movement) => {
+        if (socket.type === "robot") {
+            console.log("Error move : robot ${socket.id} cannot send move command");
+            socket.emit("move error", "A robot cannot send move command");
+            return;
+        }
+        if (socket.isControlling === true) {
+            socket.to(socket.controledRobot).emit("move", movement); // move robot
+        } else if (socket.controledRobot === "") {
+            console.log("Error move : client ${socket.id} is not connected to a robot");
+            socket.emit("move error", "You must be connected to a robot first and be master");
+        } else {
+            console.log("Error move : client ${socket.id} is connected to robot ${socket.controledRobot} but not master so must wait");
+            socket.emit("move error", "You must wait to be master");
+        }
+    })
 
 });
 
