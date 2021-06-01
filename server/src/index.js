4a269cfbf94aa7fee9863bb08b6b49d46af958be
Hugo <hugo.berthome@epitech.eu>
2021-06-01 11:34:23 +0900

    Add environment variable

--- server/src/index.js
+++ server/src/index.js
@@ -127,6 +127,6 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log('listening on *:3000');
 });
